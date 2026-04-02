77645a40782e2adde520f800e8faec2395d6b90e
Vincent Todd Andrews <vandr023@fiu.edu>
2026-04-01 23:28:53 -0400

    Initial Commit

---
+++ FlowFit/FlowFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowFitApp.swift
+//  FlowFit
+//
+//  Created by Vincent  on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct FlowFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
