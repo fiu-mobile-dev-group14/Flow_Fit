60c084b33c87f9dd3de99bb125295485ed98a8f9
Javier Gil <javigq@gmail.com>
2026-04-08 19:09:38 -0400

    Add progress view

---
+++ FlowFit/ProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView.swift
+//  FlowFit
+//
+//  Created by Javier Gil on 4/8/26.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressView()
+}
